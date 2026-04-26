0b17d01083522a98b04a4d374ec8152def491ac5
y-shi23 <y-shi23tsinghua@outlook.com>
2026-04-26 10:43:55 +0800

    chore: bump version to 0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.3.6"
+        versionName = "0.3.7"
 
         ndk {
             abiFilters.addAll(listOf("arm64-v8a"))
